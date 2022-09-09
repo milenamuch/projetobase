57b2b3e1b70f1f80ad3f727ae89da2880e4bde1a
Milena Madeira <milenamuchenski@gmail.com>
2022-09-09 20:55:16 -0300

    projeto base

--- footer.php
+++ footer.php
@@ -1,4 +1,31 @@
+<footer>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-4">
+                <h5>Fale comigo</h5>
+                <p>Escreva milena@gmail.com</p>
+            </div>
+            <div class="col-4">
+                <h5>Escreva pra mim</h5>
+                        <p>Rua Visconde de Taunay</p>
+            </div>
+            <div class="col-4">
+                <h5>Siga-me</h5>
+                <p>
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-linkedin"></i>
+                </p>
+
+            </div>
+        </div>
+    </div>
+
+</footer>
+
 
 <script src="assets/js/app.js"></script>
 </body>
+
 </html>
---
+++ sobre.php
@@ -0,0 +1,20 @@
+<?php 
+include "header.php";
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <img src="https://picsum.photos/id/2109/600/300" alt="" class="img-fluid">
+        </div>
+        <div class="col-6 align-center">
+            <h1>Bem vind_</h1>
+            <p>Bem vind_ a página do Senac, a empresa que melhor realiza graduações em tecnologia</p>
+            <p>Com a melhor infraestrutura, temos a internet banda larga mais fabulosa do país</p>
+        </div>
+    </div>
+</div>
+
+<?php
+include "footer.php";
+?>
